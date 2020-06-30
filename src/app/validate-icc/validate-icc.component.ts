9f21d076d57bef8bdc6189cfb58ec625fd882d19
hiddehs <hidde.schultze@gmail.com>
2020-06-30 16:32:41 +0200

    Step scaffholding + step 1 + step 2

---
+++ src/app/validate-icc/validate-icc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-icc',
+  templateUrl: './validate-icc.component.html',
+  styleUrls: ['./validate-icc.component.scss']
+})
+export class ValidateIccComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/validate-icc/validate-step2/validate-step2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-step2',
+  templateUrl: './validate-step2.component.html',
+  styleUrls: ['./validate-step2.component.scss']
+})
+export class ValidateStep2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
