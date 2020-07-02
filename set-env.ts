00297d97a0bae282f19d1f4dd8e1c43b217d03f2
hiddehs <hidde.schultze@gmail.com>
2020-07-02 15:24:52 +0200

    Basic authorization local via .env

---
+++ set-env.ts
@@ -0,0 +1,21 @@
+const fs = require("fs")
+// Configure Angular `environment.ts` file path
+const targetPath = './src/environments/environment.prod.ts';
+// Load node modules
+const colors = require('colors');
+require('dotenv').load();
+// `environment.ts` file structure
+const envConfigFile = `export const environment = {
+      production: false,
+      appName: "{appName}",
+      apiUrl: 'http://localhost:5006',
+      auth: '${process.env.user}:${process.env.password}',
+};
+`;
+fs.writeFile(targetPath, envConfigFile, function (err) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+});
