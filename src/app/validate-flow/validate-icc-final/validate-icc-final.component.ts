abad26a43370e0ce9befdd1873da47ebeaa4ec8a
hiddehs <hidde.schultze@gmail.com>
2020-08-11 11:36:01 +0200

    Date of symptoms in final component

--- src/app/validate-flow/validate-icc-final/validate-icc-final.component.ts
+++ src/app/validate-flow/validate-icc-final/validate-icc-final.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, LOCALE_ID, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {DatePipe} from "@angular/common";
 
 @Component({
     selector: 'app-validate-icc-final',
@@ -8,12 +9,23 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ValidateIccFinalComponent implements OnInit {
     public success = false;
+    public symptomsDate: Date;
+    private datePipe: DatePipe;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(@Inject(LOCALE_ID) private locale: string, private route: ActivatedRoute) {
+        this.datePipe = new DatePipe(locale);
         if (route.snapshot.queryParams.success) {
             this.success = route.snapshot.queryParams.success;
             window.history.pushState(null, null, 'validate_final');
         }
+        if (route.snapshot.queryParams.symptomsDate) {
+            this.symptomsDate = route.snapshot.queryParams.symptomsDate;
+        }
+    }
+
+    public friendlySymptomsDate(): string {
+        console.log(this.symptomsDate);
+        return this.datePipe.transform(this.symptomsDate, 'EEEE M MMMM');
     }
 
     ngOnInit(): void {
