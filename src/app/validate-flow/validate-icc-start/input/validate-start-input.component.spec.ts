c82ea9fb75ada1f62db7b45f72470e4ea627f3fb
hiddehs <hidde.schultze@gmail.com>
2020-08-17 11:26:18 +0200

    Add tests for getDayAgo

--- src/app/validate-flow/validate-icc-start/input/validate-start-input.component.spec.ts
+++ src/app/validate-flow/validate-icc-start/input/validate-start-input.component.spec.ts
@@ -1,9 +1,11 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ValidateStartInputComponent} from './validate-start-input.component';
-import {ActivatedRoute, Router} from "@angular/router";
-import {LabConfirmService} from "../../../services/lab-confirm.service";
+import {ActivatedRoute} from "@angular/router";
 import {of} from "rxjs";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {AppConfigService} from "../../../services/app-config.service";
 
 describe('ValidateStartInputComponent', () => {
     let component: ValidateStartInputComponent;
@@ -12,12 +14,11 @@ describe('ValidateStartInputComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             providers: [
-                Router,
-                LabConfirmService,
+                AppConfigService,
                 {
                     provide: ActivatedRoute,
                     useValue: {
-                        queryParams: of({
+                        paramMap: of({
                             get: v => {
                                 return {id: 123};
                             }
@@ -25,6 +26,7 @@ describe('ValidateStartInputComponent', () => {
                     }
                 }
             ],
+            imports: [RouterTestingModule, HttpClientTestingModule],
             declarations: [ValidateStartInputComponent]
         })
             .compileComponents();
@@ -38,5 +40,23 @@ describe('ValidateStartInputComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+
+    it('getDayAgo returns date on x days ago', () => {
+        const daysInPast = 2;
+        // testdate = 31 dec 2020
+        const inputDate = new Date();
+        inputDate.setUTCFullYear(2020)
+        inputDate.setUTCMonth(11)
+        inputDate.setUTCDate(31)
+        inputDate.setUTCHours(4)
+        inputDate.setUTCMinutes(2)
+        inputDate.setUTCSeconds(2)
+
+        const expectedDate = new Date(2020, 11, 29, 0, 0, 0);
+        const result = component.getDayAgo(daysInPast, inputDate);
+
+        expect(result).toEqual(expectedDate);
     });
 });
