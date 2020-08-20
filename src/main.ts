470e22bb943a24951deb9e625457b68894cddc69
hiddehs <hidde.schultze@gmail.com>
2020-08-20 13:57:14 +0200

    Double qutoes removal

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from "./environments/environment";
-import {enableProdMode} from "@angular/core";
+import {enableProdMode} from '@angular/core';
 
 export function getBaseUrl() {
     return document.getElementsByTagName('base')[0].href;
