409cc9a917c68927e66d23f1ccbad2bd87efe244
hiddehs <hidde.schultze@gmail.com>
2020-08-20 11:07:23 +0200

    Fixed -2 symptomsdate on final screen

--- src/app/validate-flow/validate-icc-final/validate-icc-final.component.ts
+++ src/app/validate-flow/validate-icc-final/validate-icc-final.component.ts
@@ -19,12 +19,14 @@ export class ValidateIccFinalComponent implements OnInit {
             window.history.pushState(null, null, 'validate_final');
         }
         if (route.snapshot.queryParams.symptomsDate) {
-            this.symptomsDate = route.snapshot.queryParams.symptomsDate;
+            this.symptomsDate = new Date(route.snapshot.queryParams.symptomsDate * 1);
         }
     }
 
-    public friendlySymptomsDate(): string {
-        return this.datePipe.transform(this.symptomsDate, 'EEEE d MMMM');
+    public friendlySymptomsDate(offset: number = 2): string {
+        let date = new Date(this.symptomsDate);
+        date.setDate(date.getDate() - offset);
+        return this.datePipe.transform(date, 'EEEE d MMMM');
     }
 
     ngOnInit(): void {
