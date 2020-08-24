bc9efadf9267aacfdad2391d6786d83d0ac35832
hiddehs <hidde.schultze@gmail.com>
2020-08-24 12:14:02 +0200

    Completed services tests for labConfirm

--- src/app/services/lab-confirm.service.spec.ts
+++ src/app/services/lab-confirm.service.spec.ts
@@ -2,20 +2,51 @@ import {TestBed} from '@angular/core/testing';
 import {LabConfirmService} from './lab-confirm.service';
 import {HttpClient, HttpHandler} from '@angular/common/http';
 import {AppConfigService} from './app-config.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
+import {LabVerifyService} from "./lab-verify.service";
+import {AppConfigTestService} from "./app-config.test.service";
+import {AuthenticationService} from "./authentication.service";
+import {AuthenticationTestService} from "./authenticationTestService";
 
 describe('LabConfirmServiceService', () => {
     let service: LabConfirmService;
+    let httpTestingController: HttpTestingController;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule, RouterTestingModule],
-            providers: [AppConfigService, LabConfirmService, HttpClient, HttpHandler]
+            providers: [LabConfirmService, {
+                provide: AppConfigService,
+                useClass: AppConfigTestService,
+            }, {provide: AuthenticationService, useClass: AuthenticationTestService}]
         });
+        let appConfigService = TestBed.inject(AppConfigService);
+        appConfigService.loadAppConfig();
+
+        httpTestingController = TestBed.inject(HttpTestingController);
         service = TestBed.inject(LabConfirmService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+
+    it('labConfirm returned Observable should match the right data', () => {
+        const mockLabConfirmationId = ["0","0","0","0","0","0"];
+        const mockDateOfSymptomsOnset = new Date().toISOString();
+        const mockResponse = {
+            active: true,
+            pollToken: "first_polltoken_123"
+        };
+
+        service.confirmLabId(mockLabConfirmationId, mockDateOfSymptomsOnset).subscribe(result => {
+            expect(result.active).toEqual(true);
+            expect(result.pollToken).toEqual("first_polltoken_123");
+        });
+
+        const req = httpTestingController.expectOne('http://coronamelder.test/CaregiversPortalApi/v1/labconfirm');
+        req.flush(mockResponse);
     });
 });
