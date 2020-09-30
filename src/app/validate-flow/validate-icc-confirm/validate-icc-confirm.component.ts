4d0bfe432ca6e140047148d7476d42ec5275e74c
hiddehs <hidde.schultze@gmail.com>
2020-09-30 16:28:12 +0200

    Fix dateOfsymptoms passthrough and final page

--- src/app/validate-flow/validate-icc-confirm/validate-icc-confirm.component.ts
+++ src/app/validate-flow/validate-icc-confirm/validate-icc-confirm.component.ts
@@ -9,17 +9,14 @@ import {DatePipe} from '@angular/common';
 })
 export class ValidateIccConfirmComponent implements OnInit {
     public success = false;
-    public symptomsDate: Date;
+    public symptomsDate: number;
     private datePipe: DatePipe;
 
     constructor(@Inject(LOCALE_ID) private locale: string, private router: Router, private route: ActivatedRoute) {
         this.datePipe = new DatePipe(locale);
         if (route.snapshot.queryParams.symptomsDate) {
             window.history.pushState(null, null, 'validate/confirm');
-            const queryParamSymptomsDate = parseInt(route.snapshot.queryParams.symptomsDate, 0);
-            if (Number.isInteger(queryParamSymptomsDate)) {
-                this.symptomsDate = new Date(queryParamSymptomsDate);
-            }
+            this.symptomsDate = parseInt(route.snapshot.queryParams.symptomsDate, 0);
         }
     }
 
@@ -32,15 +29,13 @@ export class ValidateIccConfirmComponent implements OnInit {
         }
     }
 
-
-    public friendlySymptomsDate(offset: number = 2): string {
-        if (!this.symptomsDate) {
-            return '';
-        }
-        const date = new Date(this.symptomsDate);
-        date.setDate(date.getDate() - offset);
-        return this.datePipe.transform(date, 'EEEE d MMMM');
+    goToValidateFinal() {
+        this.router.navigate(['/validate_final'], {
+            queryParams: {
+                success: true,
+                symptomsDate: this.symptomsDate
+            }
+        });
     }
-
 
 }
